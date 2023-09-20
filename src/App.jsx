7fc2f91803d79c525201af7c80ccb27060e12677
Chhorm Bunthai <bunthaicambo89@gmail.com>
2023-09-20 16:47:12 +0700

    done searchbar for creating task

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import TaskCreating from './components/TaskCreating';
+import { useState } from 'react';
 
 function App() {
-
+  const [title, setTitle] = useState([]);
+  const createTask = (term) => {
+    const updatedTasks = [
+      ...title,
+      { id: Math.round(Math.random() * 10000),term },
+    ];
+    setTitle(updatedTasks);
+  };
   return (
     <>
-      hello
+      <TaskCreating onSubmit={createTask}/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/TaskCreating.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+function TaskCreating({onSubmit}) {
+    const [word, setWord] = useState('')
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(word);
+        setWord('')
+    }
+    const handleChange = (e) =>{
+        setWord(e.target.value)
+        console.log(word) 
+    }
+  return (
+    <div className='input-form'>
+        <form onSubmit={handleSubmit} className='form'>
+            <input value={word} type="text" onChange={handleChange} placeholder='Add your tasks'/>
+        </form>
+    </div>
+  )
+}
+
+export default TaskCreating
